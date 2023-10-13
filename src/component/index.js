305e056f442195c6aee6180aa974d47f400f8f1a
hevi <89172641+SurafelHeavy@users.noreply.github.com>
2023-10-13 06:33:35 -0700

    Modern bank  Ui

---
+++ src/component/index.js
@@ -0,0 +1,24 @@
+import Navbar from "./navbar";
+import Billing from "./Billing";
+import CardDeal from "./cardDeal";
+import Business from "./Business";
+import Clients from "./client";
+import CTA from "./CTA";
+import Stats from "./stat";
+import Footer from "./footer";
+import Testimonials from "./testimonials";
+import Hero from "./hero";
+
+export {
+  Navbar,
+  Hero,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials
+ 
+};
